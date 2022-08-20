560678ed6c502caf531715d821bf5cdeac3d0fa6
Bonsai <puskajleri@gmail.com>
2022-08-20 17:18:40 +0200

    Added index.php and initialized composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'radmel/cotinus',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '38109a6b0ef43b748f95ea22adc4896e1c5525be',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'radmel/cotinus' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '38109a6b0ef43b748f95ea22adc4896e1c5525be',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
